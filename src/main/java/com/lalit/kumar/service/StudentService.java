ea41033f5b5d214218ba874d4fdf6666ad82c4dc
Lalit Kumar <lalit.kumar@ramsys.sa>
2025-06-06 00:31:51 +0530

    Lalit changes

--- src/main/java/com/lalit/kumar/service/StudentService.java
+++ src/main/java/com/lalit/kumar/service/StudentService.java
@@ -6,9 +6,14 @@ import com.lalit.kumar.entity.Student;
 
 public interface StudentService {
 	Student saveStudent(Student student);
+
 	List<Student> saveAllStudents(List<Student> students);
+
 	List<Student> getAllStudents();
+
 	Student getStudentById(Long id);
+
 	Student updateStudent(Long id, Student updatedStudent);
+
 	void deleteStudent(Long id);
 }
