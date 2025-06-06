40e4fc6765aa0896509fdec708dc6993a0677d3f
Lalit Kumar <lalit.kumar@ramsys.sa>
2025-06-06 15:40:50 +0530

    Email chanage

--- src/main/java/com/lalit/kumar/config/SwaggerConfig.java
+++ src/main/java/com/lalit/kumar/config/SwaggerConfig.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Student Management API", description = "API for managing student data (CRUD operations)", version = "1.0", contact = @Contact(name = "Lalit Kumar", email = "lalit@example.com"), license = @License(name = "Apache 2.0", url = "http://springdoc.org")), servers = {
+@OpenAPIDefinition(info = @Info(title = "Student Management API", description = "API for managing student data (CRUD operations)", version = "1.0", contact = @Contact(name = "Lalit Kumar", email = "lalitkumarcs4@gmail.com"), license = @License(name = "Apache 2.0", url = "http://springdoc.org")), servers = {
 		@Server(url = "http://localhost:8083", description = "Local Server") }, tags = {
 				@Tag(name = "Student", description = "Operations related to student entity") })
 public class SwaggerConfig {
